9400bdb223d875dbb2f83bafd01e99663d2af0ef
LiamSalass <liamsalass@yahoo.com>
2023-10-13 17:24:41 -0400

    minor fix to datatype

--- ransac.py
+++ ransac.py
@@ -23,7 +23,12 @@ def centerest(point_list, radius_list):
     return X[0], X[1], X[2]
 
 
-
+vote = np.dtype([
+    ('mse', np.float64),
+    ('x', np.float64),
+    ('y', np.float64),
+    ('z', np.float64)
+])
 
 def random_centerest(xyz, radial_list, iterations):
     vote_list = np.zeros(iterations, dtype=vote)
