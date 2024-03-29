221de119e894e58daa08b2dbd58836ab9ce88faf
LiamSalass <liamsalass@yahoo.com>
2024-03-29 10:17:50 -0400

    Delete unused result files and update epsilon value in eps_test.py

--- eps_test.py
+++ eps_test.py
@@ -989,7 +989,7 @@ if __name__ == "__main__":
     
     opts = parser.parse_args()   
 
-    output_dir = 'logs/itr_test_on_train_set/'
+    output_dir = 'logs/eps_test_on_train_set/'
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
@@ -1001,7 +1001,7 @@ if __name__ == "__main__":
         opts.frontend = 'RANSAC_refine'
 
     if opts.dataset == 'lm':
-        eps = 0.03
+        eps = 0.01
         eps_list = []
         offset_list = []
         offset_list_w_refinement = []
@@ -1012,7 +1012,8 @@ if __name__ == "__main__":
         fps_list_w_refinement = []
         iteration_list = []
         iterations = [5, 10, 15, 20, 30, 40, 50, 60, 100, 200, 400]
-        for itr in iterations:
+        while eps < 0.1:
+            itr = 200
             print("Current eps: ", eps)
             offset, stds, fps, offset_w_refinement, std_w_refinement, fps_w_refinement, obj_size, inlier  = estimate_6d_pose_lm(opts, eps, itr)
             iteration_list.append(itr)
@@ -1113,6 +1114,8 @@ if __name__ == "__main__":
 
             with open(os.path.join(output_dir, 'results.txt'), 'a') as f:
                 f.write(f"Epsilon: {eps}, Iterations {itr}, Offset: {offset}, Offset with Refinement: {offset_w_refinement}, Std: {stds}, Std with Refinement: {std_w_refinement}, Average Object Size {obj_size}, Inlier Count: {inlier}, Inlier Ratio: {inlier/obj_size}, FPS: {fps}, FPS with Refinement: {fps_w_refinement}\n")
+
+            eps += 0.01
 
     if opts.dataset == 'lmo':
         estimate_6d_pose_lmo(opts)
